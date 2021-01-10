f86e931d2101dc3706475a24310bbb40a34902a3
Matt Bianco <matthewbianco@protonmail.com>
2021-01-10 10:14:18 -0500

    added questions to the game

---
+++ assets/JS/game.js
@@ -0,0 +1,34 @@
+const question = document.getElementById("question");
+const choices = Array.from(document.getElementByClassName("choice-text"));
+
+let currentQuestion = {};
+let accecptingAnswers = true;
+let score = 0;
+let questionCounter = 0;
+let availableQuestions = [];
+
+let questions = [
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+]
