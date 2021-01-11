8a86a783adf12028ec8ca3938af73553319a5e01
Matt Bianco <matthewbianco@protonmail.com>
2021-01-10 20:23:31 -0500

    added saved score data to the final score and improved notes

--- assets/JS/end.js
+++ assets/JS/end.js
@@ -1,14 +1,36 @@
 const username = document.getElementById('username');
 const saveScoreBtn = document.getElementById('saveScoreBtn');
 const finalScore = document.getElementById('finalScore');
-const mostRecentScore = localStorage.getItem('mostRecentScore')
+const mostRecentScore = localStorage.getItem('mostRecentScore');
+
+const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
+
+const MAX_HIGH_SCORES = 3;
+
+// TAKES USER QUIZ SCORE AND MAKES IT THE FINAL SCORE 
 finalScore.innerText = mostRecentScore;
 
-username.addEventListener("keyup", () => {
-    console.log(username.value);
+
+// KEEP SAVE SCORE BUTTON DISABLED UNTIL A STRING IS ENTERED INTO THE "USERNAME" BOX
+username.addEventListener('keyup', () => {
     saveScoreBtn.disabled = !username.value;
 });
+// KEEP SAVE SCORE BUTTON DISABLED UNTIL A STRING IS ENTERED INTO THE "USERNAME" BOX END
 
-saveHighScore = e => {
+
+// SAVE HIGH SCORE TO LOCAL STORAGE 
+saveHighScore = (e) => {
     e.preventDefault();
-}
+
+    const score = {
+        score: mostRecentScore,
+        name: username.value,
+    };
+    highScores.push(score);
+    highScores.sort((a, b) => b.score - a.score);
+    highScores.splice(3);
+
+    localStorage.setItem('highScores', JSON.stringify(highScores));
+    window.location.assign('/');
+};
+// SAVE HIGH SCORE TO LOCAL STORAGE END
